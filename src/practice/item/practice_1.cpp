ee5f337056cdcc524bbad8026c7fe3e4a141eb05
R2Road <studior2road@gmail.com>
2022-12-18 22:17:41 +0900

    add test : practice_1 - _1

--- src/practice/item/practice_1.cpp
+++ src/practice/item/practice_1.cpp
@@ -1,6 +1,6 @@
 #include "practice_1.h"
 
-#include <conio.h> // _getch
+#include <conio.h>
 
 #include "r2cm/r2cm_ostream.h"
 
@@ -19,11 +19,61 @@ namespace practice_1
 		{
 			std::cout << r2cm::split;
 
-			std::cout << r2cm::tab2 << "Do Something" << r2cm::linefeed;
+			{
+				const int BUFFER_SIZE = 256;
+				char linebuffer[BUFFER_SIZE];
+
+				double result;
+				double sum_normal = 0.f;
+				double sum_pow2 = 0.f;
+
+				while( fgets( linebuffer, BUFFER_SIZE, stdin ) )
+				{
+					//
+					// [Q] : Exit
+					//
+					if( 'q' == linebuffer[0] && '\n' == linebuffer[1] )
+					{
+						break;
+					}
+
+					//
+					// [Enter] : Skip
+					//
+					if( '\n' == linebuffer[0] )
+					{
+						printf( "[skip] : Input <Enter>" "\n\n" );
+						continue;
+					}
+
+					if( sscanf_s( linebuffer, "%lf", &result ) )
+					{
+						sum_normal += result;
+						sum_pow2 += ( result * result );
+						printf(
+							"sum normal : %lf"
+							"   "
+							"sum pow2   : %lf"
+							"\n"
+							, sum_normal, sum_pow2
+						);
+					}
+					else
+					{
+						printf(
+							"[failed input]"
+							" : "
+							"%s"
+							"\n"
+							, linebuffer
+						);
+					}
+				}
+			}
 
 			std::cout << r2cm::split;
 
-			return r2cm::eItemLeaveAction::Pause;
+			return r2cm::eItemLeaveAction::None;
 		};
 	}
 }
