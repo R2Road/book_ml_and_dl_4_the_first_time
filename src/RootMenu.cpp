4def3941bae7ce1de97b58a41b5e93f95efa0b88
R2Road <studior2road@gmail.com>
2025-04-25 15:10:49 +0900

    adjust key map

--- src/RootMenu.cpp
+++ src/RootMenu.cpp
@@ -41,12 +41,32 @@ r2tm::WriteFunctionT RootMenu::GetWriteFunction() const
 	return []( r2tm::MenuProcessor* ret )
 	{
 		ret->AddItem( '1', example_01_01::Do() );
-		ret->AddItem( '2', example_02_02::LoadData() );
-		ret->AddItem( '3', example_02_02::Do() );
-		ret->AddItem( '4', example_02_03::Do() );
-		ret->AddItem( '5', example_03_01::Do() );
-		ret->AddItem( '6', example_03_02::Crossing() );
-		ret->AddItem( '7', example_03_02::Do() );
+
+
+		ret->AddLineFeed();
+
+
+		ret->AddItem( 'q', example_02_02::LoadData() );
+		ret->AddItem( 'w', example_02_02::Do() );
+
+
+		ret->AddLineFeed();
+
+
+		ret->AddItem( 'e', example_02_03::Do() );
+
+
+		ret->AddLineFeed();
+
+
+		ret->AddItem( 'a', example_03_01::Do() );
+
+
+		ret->AddLineFeed();
+
+
+		ret->AddItem( 's', example_03_02::Crossing() );
+		ret->AddItem( 'd', example_03_02::Do() );
 
 
 
